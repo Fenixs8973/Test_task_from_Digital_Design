33f4fb1f2c076626d1867d9f3792d53b73747666
Fenixs8973 <61942811+Fenixs8973@users.noreply.github.com>
2023-05-23 20:19:13 +0300

    ошибка с возвратом результата

---
+++ dllTextProcess/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace TextProcessingDll
+{
+    public class TextStatistics
+    {
+        struct Element
+        {
+            private string Word { get; set; }
+            private int Value { get; set; }
+            public string GetWord() { return Word; }
+            public void SetWord(string Word) { this.Word = Word; }
+            public int GetValue() { return Value; }
+            public void SetValue(int Value) { this.Value = Value; }
+            public Element(string word, int value)
+            {
+                this.Word = word;
+                this.Value = value;
+            }
+        }
+        private Dictionary<string, int> Processtng(string text)
+        {
+            //string text = WarAndPeace.ToString();
+
+            //Словарь, куда в начале будут добавляться слова и их количество
+            Dictionary<string, int> rating = new Dictionary<string, int>();
+
+            //Выделяет слово
+            Regex regex = new Regex(@"\w+");
+
+            MatchCollection matches = regex.Matches(text);
+            int ValueLines = 0;
+
+            //Записывает в cловарь новые слова
+            foreach (Match match in matches)
+            {
+                //Если слово уже есть в словаре, то переходит к следующему
+                if (rating.ContainsKey(match.ToString()) != false)
+                {
+                    rating[match.ToString()]++;
+                }
+                //Если слова нету, то добавляет новый элемент в словарь
+                else
+                {
+                    rating.Add(match.ToString(), 1);
+                    ValueLines++;
+                }
+            }
+
+            Element[] element = new Element[ValueLines];
+            int h = 0;
+
+            //Словарь переписывается в массив для сортировки
+            foreach (var g in rating)
+            {
+                element[h].SetWord(g.Key);
+                element[h].SetValue(g.Value);
+                h++;
+            }
+
+            //Сортировка массива
+            element = QuickSort(element, 0, element.Length - 1);
+
+            foreach (var i in element)
+            {
+                rating.TryAdd(i.GetWord(), i.GetValue());
+            }
+
+            return rating;
+        }
+
+        //Блок сортировка
+        //Нахождение опорного элемента
+        static int FindPivot(Element[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+            Element temp = new Element();
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i].GetValue() > array[maxIndex].GetValue())
+                {
+                    pivot++;
+                    temp.SetValue(array[pivot].GetValue());
+                    temp.SetWord(array[pivot].GetWord());
+
+                    array[pivot].SetValue(array[i].GetValue());
+                    array[pivot].SetWord(array[i].GetWord());
+
+                    array[i].SetValue(temp.GetValue());
+                    array[i].SetWord(temp.GetWord());
+                }
+            }
+            pivot++;
+
+            temp.SetValue(array[pivot].GetValue());
+            temp.SetWord(array[pivot].GetWord());
+
+            array[pivot].SetValue(array[maxIndex].GetValue());
+            array[pivot].SetWord(array[maxIndex].GetWord());
+
+            array[maxIndex].SetValue(temp.GetValue());
+            array[maxIndex].SetWord(temp.GetWord());
+
+            return pivot;
+        }
+
+        //Быстра сортировка
+        static Element[] QuickSort(Element[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivot = FindPivot(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivot - 1);
+            QuickSort(array, pivot + 1, maxIndex);
+
+            return array;
+        }
+    }
+
+}
---
+++ exe/TextStatistics.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Reflection;
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        string path = @"War_and_peace.txt";
+
+        string WarAndPeace = File.ReadAllText(path);
+
+        TextProcessingDll.TextStatistics TS = new TextProcessingDll.TextStatistics();
+        
+        var type = typeof(TextProcessingDll.TextStatistics);
+
+        MethodInfo mi = type.GetMethod("Processtng", BindingFlags.NonPublic | BindingFlags.Instance);//.GetMethod("Processtng");
+
+        object[] text = new object[] { WarAndPeace.ToCharArray() };
+
+        //Invoke - вызов метода
+        Dictionary<string, int> result = null;
+
+        try
+        {
+            result = mi.Invoke(TS, new object[] { WarAndPeace.ToCharArray() }) as Dictionary<string, int>;
+        }
+        catch
+        {
+
+        }
+
+        path = @"Result.txt";
+        //Переписывание отсортированного массива в итоговый файл
+        foreach (var i in result)
+        {
+            string KeyValue = $"{i.Key,-20} {i.Value}\n";
+            File.AppendAllText(path, KeyValue);
+        }
+        Console.WriteLine("Программа закончила выполнение");
+    }
+}
